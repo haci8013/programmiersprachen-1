d0092f45ad6f75d7e612a3549ec6e3e67f4fb308
haci8013 <Kraska.Dorothea@gmail.com>
2018-04-18 19:05:57 +0200

    small change

--- source/tests.cpp
+++ source/tests.cpp
@@ -5,3 +5,12 @@ int main(int argc, char* argv[])
 {
   return Catch::Session().run(argc, argv);
 }
+
+int andom(){
+  return 5;
+}
+
+TEST_CASE("describe_gcd", "[gcd]"){
+  REQUIRE(andom() == 2);
+
+}
